0df46fa42381bc7f072697193be16f9a86adccd0
Alejae1998 <alejagr1998@gmail.com>
2022-08-16 10:59:43 -0700

    installing dependencies

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,37 +2,27 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const UserService = require('../lib/services/UserService');
+
 
 const mockUser = {
   email: 'test@example.com',
   password: '123456',
 };
 
-// const registerAndLogin = async (userProps = {}) => {
-//   const password = userProps.password ?? mockUser.password;
-//   const agent = request.agent(app);
-//   const user = await UserService.create({ ...mockUser, ...userProps });
-
-//   const { email } = user;
-//   await agent.post('/api/v1/users/sessions').send({ email, password });
-//   return [agent, user];
-// };
-
-
 describe('users routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  // it('POST /api/v1/users/ creates a new user', async  () => {
-  //   const res = await (await request(app).post('/api/v1/users')).setEncoding(mockUser);
-  //   const { email } = mockUser;
-  //   expect(res.body).toEqual({
-  //     id: expect.any(String),
-  //     email,
-  //   });
+  it('#POST /users should create a new user if none exists', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+
+    expect(res.status).toBe(200);
+    expect(res.body.message).toEqual(
+      'Successfully logged in as a new user!'
+    );
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
 });
-afterAll(() => {
-  pool.end();
-});
-// });
