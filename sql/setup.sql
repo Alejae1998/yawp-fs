bf4cb9f68d8417801bc6e1953faeba8d3793a0c6
Alejae1998 <alejagr1998@gmail.com>
2022-08-17 10:01:05 -0700

    all users test passing, one restaurant test pass, adding second test and fixing my database

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,9 +2,9 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
 DROP TABLE IF EXISTS yawp_users CASCADE;
-DROP TABLE IF EXISTS restaurants CASCADE;
-DROP TABLE IF EXISTS reviews CASCADE;
-DROP TABLE IF EXISTS reviews_restaurants CASCADE;
+DROP TABLE IF EXISTS yawp_restaurants CASCADE;
+DROP TABLE IF EXISTS yawp_reviews CASCADE;
+
 
 CREATE TABLE yawp_users (
 id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -12,28 +12,23 @@ id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     password_hash VARCHAR NOT NULL
 );
 
-CREATE TABLE restaurants (
+CREATE TABLE yawp_restaurants (
 id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 name VARCHAR NOT NULL,
-cusine VARCHAR NOT NULL
+type TEXT
 );
 
-CREATE TABLE reviews (
+CREATE TABLE yawp_reviews (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  yawp_user_id INT,
-  restaurants_id BIGINT,
-  FOREIGN KEY (yawp_user_id) references yawp_users(id),
-  stars int not null,
-  detail VARCHAR
+  user_id BIGINT,
+  restaurant_id BIGINT,
+  stars INT,
+  detail VARCHAR(255) NOT NULL,
+  FOREIGN KEY (user_id) references yawp_users(id),
+  FOREIGN KEY (restaurant_id) references yawp_restaurants(id)
 );
 
-CREATE TABLE reviews_restaurants (
-  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-reviews_id int,
-restaurants_id int,
-FOREIGN KEY (reviews_id) references reviews(id),
-FOREIGN KEY (restaurants_id) references restaurants(id)
-);
+
 
 INSERT INTO yawp_users (email, password_hash) VALUES
 ('bikini@bottom.com', 'fakePasswordHash'),
@@ -42,25 +37,20 @@ INSERT INTO yawp_users (email, password_hash) VALUES
 ('dexters@lab.com', 'fakePasswordHash'),
 ('test2@example.com', '$2b$10$ppw7JtE6dIWepG7/a32TsO2icC6/SJX7O3jjzSlFK8zhL4LuYe5AG');
 
-INSERT INTO restaurants (name, cusine) VALUES 
+INSERT INTO yawp_restaurants (name, type) VALUES 
 ('Cubo', 'Cuban'),
 ('K-Town', 'Korean'),
 ('Santorini', 'Greek'),
 ('QBacano', 'Colombian'),
 ('Valencia', 'Spanish');
 
-INSERT INTO reviews (stars, detail) VALUES 
-(5, 'Best ropa vieja in town'),
-(5, 'A little bit to spendy but best korean bbq in pdx'),
-(1, 'Tzatziki was really bad'),
-(3, 'service is really bad here'),
-(5, 'Omg paella here is amazing!');
+INSERT INTO yawp_reviews (restaurant_id, user_id, stars, detail) VALUES 
+(1, 1, 5, 'Best ropa vieja in town'),
+(2, 2, 5, 'A little bit to spendy but best korean bbq in pdx'),
+(3, 3, 1, 'Tzatziki was really bad'),
+(4, 4, 3, 'service is really bad here'),
+(5, 5, 5, 'Omg paella here is amazing!');
 
-INSERT INTO reviews_restaurants (restaurants_id, reviews_id) VALUES 
-(1,1),
-(2,2),
-(3,3),
-(4,4),
-(5,5);
+
 
 
